723ad371f555c99699836b1820aa30503875436c
Marcos Iglesias <golodhros@gmail.com>
2018-04-02 22:21:49 -0700

    First basic test

--- src/heatmap.js
+++ src/heatmap.js
@@ -1,7 +1,37 @@
-import d3 from 'd3';
+const d3 = require('d3');
 
-const heatmap = {
-    name: 'heatmap'
+function heatmap() {
+    const width = 600;
+    const height = 400;
+    const margin = {
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10
+    };
+
+    let svg;
+    let chartWidth;
+    let chartHeight;
+
+    function exports(_selection) {
+        _selection.each(function(_data) {
+            chartWidth = width - margin.left - margin.right;
+            chartHeight = height - margin.top - margin.bottom;
+
+            if (!svg) {
+                svg = d3.select(this)
+                    .append('svg')
+                      .classed('heatmap', true);
+            }
+
+            svg
+                .attr('width', width)
+                .attr('height', height);
+        });
+    }
+
+    return exports;
 };
 
 export default heatmap;
--- src/heatmap.test.js
+++ src/heatmap.test.js
@@ -1,13 +1,39 @@
-import d3 from 'd3';
-import heatmap from './heatmap';
+const d3 = require('d3');
+
+const heatmap = require('./heatmap').default;
+
+const data = [];
 
 describe('Heatmap', () => {
-    let heatmapChart = heatmap;
+    let heatmapChart = heatmap();
 
-    it('should be a heatmap', () => {
-        let expected = 'heatmap';
-        let actual = heatmapChart.name;
+    // adds an html fixture to the DOM
+    beforeEach(() => {
+        const fixture = '<div id="fixture"></div>';
 
-        expect(actual).toEqual(expected);
+        document.body.insertAdjacentHTML('afterbegin', fixture);
     });
+
+    // remove the html fixture from the DOM
+    afterEach(function() {
+        document.body.removeChild(document.getElementById('fixture'));
+    });
+
+    describe('rendering the chart', () => {
+        let container;
+
+        beforeEach(() => {
+            container = d3.select('#fixture');
+
+            container.datum(data).call(heatmapChart);
+        });
+
+        it('should be a heatmap', () => {
+            let expected = 1;
+            let actual = container.select('.heatmap').nodes().length;
+
+            expect(actual).toEqual(expected);
+        });
+    });
+
 });
